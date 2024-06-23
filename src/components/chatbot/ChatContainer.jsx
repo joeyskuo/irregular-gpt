22f80bec923c7d79d616e8d9523503133a46a08c
Joey Kuo <joeyskuo@gmail.com>
2024-06-23 15:00:03 -0700

    ChatContainer includes ChatBot

---
+++ src/components/chatbot/ChatContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBot from "react-chatbotify";
+
+const ChatContainer = () => {
+    return (
+        <div className="chat-bot-container">
+            <ChatBot/>
+        </div>
+    )
+}
+
+export default ChatContainer;
