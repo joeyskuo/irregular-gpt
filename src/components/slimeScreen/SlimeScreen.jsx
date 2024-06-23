5048e3a6562cb00fed8a5f95a36fb3afd36683aa
Joey Kuo <joeyskuo@gmail.com>
2024-06-23 14:53:43 -0700

    SlimeScreen initial markup created

---
+++ src/components/slimeScreen/SlimeScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SlimeScreen = () => {
+    return (
+        <div className="slime-screen">
+            slimeScreen!!
+        </div>
+    )
+}
+
+export default SlimeScreen;
