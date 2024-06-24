2bd13da7fb8452ee51b23ad36f14ccb84fad9c09
Joey Kuo <joeyskuo@gmail.com>
2024-06-23 17:59:04 -0700

    SlimeScreen initializes ChatContainer with controller

--- src/components/slimeScreen/SlimeScreen.jsx
+++ src/components/slimeScreen/SlimeScreen.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import './SlimeScreen.css';
+import ChatContainer from "../chatbot/ChatContainer";
+import ChatBotController from "../chatbot/ChatBotController";
 
 const SlimeScreen = () => {
     return (
         <div className="slime-screen">
-            slimeScreen!!
+            <ChatContainer controllerFunction={ChatBotController}/>
         </div>
     )
 }
