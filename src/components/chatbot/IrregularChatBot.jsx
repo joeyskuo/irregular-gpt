3957d07d3115cf0b8ddd2d282db0700198a4a486
Joey Kuo <joeyskuo@gmail.com>
2024-06-23 19:47:20 -0700

    rename function to setMessagesHandler

--- src/components/chatbot/IrregularChatBot.jsx
+++ src/components/chatbot/IrregularChatBot.jsx
@@ -6,13 +6,13 @@ const IrregularChatBot = (props) => {
     const { controllerFunction } = props;
     const { options, messages, flow } = props.uiData;
 
-    const controllerSetMessages = (messageFunction) => {
+    const setMessagesHandler = (messageFunction) => {
         controllerFunction('applyMessageFunction', messageFunction);
     }
 
 	return (
 		<>
-			<MessagesContext.Provider value={{messages: messages, setMessages: controllerSetMessages}}>
+			<MessagesContext.Provider value={{messages: messages, setMessages: setMessagesHandler}}>
 				<ChatBot options={options} flow={flow}/>
 			</MessagesContext.Provider>
 		</>
