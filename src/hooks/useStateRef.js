31579f8291aae0f332e1ca60d820d408ff47cb03
Joey Kuo <joeyskuo@gmail.com>
2024-07-02 21:48:25 -0700

    useStateRef hook created

---
+++ src/hooks/useStateRef.js
@@ -0,0 +1,15 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useStateRef = (initialValue) => {
+    
+    const [value, setValue] = useState(initialValue);
+  
+    const ref = useRef(value);
+  
+    useEffect(() => {
+      ref.current = value;
+      console.log("value has been saved as " + value);
+    }, [value]);
+  
+    return [value, setValue, ref];
+  }
