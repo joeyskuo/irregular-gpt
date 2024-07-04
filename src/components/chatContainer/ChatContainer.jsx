22680db7616f641e331cff79c89923bb37c53939
Joey Kuo <joeyskuo@gmail.com>
2024-07-03 19:03:29 -0700

    message eventHandler created with flushSync for non-batched updates

--- src/components/chatContainer/ChatContainer.jsx
+++ src/components/chatContainer/ChatContainer.jsx
@@ -3,10 +3,33 @@ import { useStateRef } from "../../hooks/useStateRef";
 import './ChatContainer.css';
 import MessageContainer from "./messageContainer/MessageContainer";
 import { MessageContext } from "../../models/messageContext/MessageContext";
+import { flushSync } from "react-dom";
 
 const ChatContainer = (props) => {
 
     const [messages, setMessages, messagesRef] = useStateRef([{role: "assistant", content: "How can Claude help you today?", messageId: "hash07ae1"}]);
+
+    const updateMessages = (data) => {
+        
+        const currentMessages = messagesRef.current;
+        const currentMessagesLength = currentMessages.length;
+
+        let latestMessage = currentMessages[currentMessages.length-1];
+        let updatedMessage = {...latestMessage};
+        let newMessages = [...currentMessages];
+
+
+        if(latestMessage.role === "user") {
+            updatedMessage.content += data;
+            newMessages[newMessages.length-1] = updatedMessage;
+
+        } else {
+            updatedMessage = {role: "user", content: "" + data, messageId: `hash000-${currentMessagesLength}`};
+            newMessages.push(updatedMessage);
+        }
+
+        flushSync(() => setMessages(newMessages));
+    }
 
     useEffect(() => {
         const sse = new EventSource("http://localhost:8080/stream", {withCredentials: false });
