a84ed6669c0486e70d3f3c7579a5daa9176014fa
Joey Kuo <joeyskuo@gmail.com>
2024-07-03 21:08:32 -0700

    PromptInput component and styling created

---
+++ src/components/chatContainer/promptInput/PromptInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './PromptInput.css';
+
+const PromptInput = () => {
+
+
+    return (
+        <div className="prompt-input-wrapper">
+            <input className="prompt-input" type="text" />
+        </div>
+    )
+}
+
+export default PromptInput;
