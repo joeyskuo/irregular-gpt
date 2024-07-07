6f2b62ba28ed0d746eec99c786d675f2d8de3dbd
Joey Kuo <joeyskuo@gmail.com>
2024-07-07 14:59:27 -0700

    Message has icon and header; set in grid format

--- src/components/chatContainer/messageContainer/Message.jsx
+++ src/components/chatContainer/messageContainer/Message.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
+import testIcon from '../../../assets/icon.svg';
+import './Message.css';
 
 const Message = (props) => {
 
     return (
         <div className={`message message--${props.message.role}`}>
-            {props.message.content}
+            <span className='message-role-icon'>
+                <img src={testIcon} />
+            </span>
+            <span className='message-content'>
+                <div className='message-content-header'>{props.message.role}</div>
+                <div className='message-content-text'>{props.message.content}</div>
+            </span>
         </div>
     )
 }
