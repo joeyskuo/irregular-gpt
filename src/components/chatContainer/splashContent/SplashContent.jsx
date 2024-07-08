7b39d6dfb12f000d5608800e6f9344e5759e0a2b
Joey Kuo <joeyskuo@gmail.com>
2024-07-07 19:55:11 -0700

    replaced Claude with I in splash text

--- src/components/chatContainer/splashContent/SplashContent.jsx
+++ src/components/chatContainer/splashContent/SplashContent.jsx
@@ -5,7 +5,7 @@ const SplashContent = () => {
 
     return (
         <div className="splash-content">
-            <div className="splash-content--prompt">How can Claude help you today?</div>
+            <div className="splash-content--prompt">How can I help you today?</div>
             <img src="/src/assets/robot.png" />
         </div>
     )
