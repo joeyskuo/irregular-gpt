ae56d3e4664fb58acae7615d6eb1ccda5ea0338d
Joey Kuo <joeyskuo@gmail.com>
2024-07-07 19:57:19 -0700

    input autofocused on page visit

--- src/components/chatContainer/promptInput/PromptInput.jsx
+++ src/components/chatContainer/promptInput/PromptInput.jsx
@@ -6,7 +6,7 @@ const PromptInput = () => {
 
     return (
         <div className="prompt-input-wrapper">
-            <input className="prompt-input" type="text" placeholder="Reply to IrregularGPT.."/>
+            <input className="prompt-input" type="text" placeholder="Reply to IrregularGPT.." autoFocus/>
         </div>
     )
 }
