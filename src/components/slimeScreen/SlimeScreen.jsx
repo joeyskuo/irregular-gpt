8aa681d48c50e06f99dc39ade127ccb32c76bf08
Joey Kuo <joeyskuo@gmail.com>
2024-07-07 17:18:34 -0700

    heading added to SlimeScreen; css file into scss

--- src/components/slimeScreen/SlimeScreen.jsx
+++ src/components/slimeScreen/SlimeScreen.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './SlimeScreen.css';
+import './SlimeScreen.scss';
 import ChatContainer from "../chatContainer/ChatContainer";
 
 const SlimeScreen = () => {
     return (
         <div className="slime-screen">
+            <div className="slime-screen--heading">IrregularGPT</div>
             <ChatContainer />
         </div>
     )
