e3e35bf9d9ab36882abe3ca23fbd7c900a821553
Joey Kuo <joeyskuo@gmail.com>
2024-07-19 19:45:00 -0700

    Message component uses TMessage; created MessageProps type

--- src/components/chatContainer/messageContainer/Message.tsx
+++ src/components/chatContainer/messageContainer/Message.tsx
@@ -1,16 +1,21 @@
+import { TMessage } from '../../../types/api';
 import './Message.css';
 
-const Message = (props) => {
+type MessageProps = {
+    message: TMessage
+}
+
+const Message = ({ message } : MessageProps) => {
 
     return (
-        <div className={`message message--${props.message.role}`}>
+        <div className={`message message--${message.role}`}>
             <span className='message-role-icon'>
-                {props.message.role === 'user' && <img src='/src/assets/userIcon.png' />}
-                {props.message.role === 'assistant' && <img src='/src/assets/robotIcon.svg' />}
+                {message.role === 'user' && <img src='/src/assets/userIcon.png' />}
+                {message.role === 'assistant' && <img src='/src/assets/robotIcon.svg' />}
             </span>
             <span className='message-content'>
-                <div className='message-content-header'>{props.message.role}</div>
-                <div className='message-content-text'>{props.message.content}</div>
+                <div className='message-content-header'>{message.role}</div>
+                <div className='message-content-text'>{message.content}</div>
             </span>
         </div>
     )
