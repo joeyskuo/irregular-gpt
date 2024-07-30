f40c61ae56f147df278d9b73f102df1f7cb4998f
Joey Kuo <joeyskuo@gmail.com>
2024-07-30 13:23:10 -0700

    nav items added to infoScreen

--- src/components/infoScreen/InfoScreen.tsx
+++ src/components/infoScreen/InfoScreen.tsx
@@ -3,6 +3,11 @@ import './InfoScreen.scss';
 const InfoScreen = () => {
     return (
         <div className="info-screen">
+            <div className='info-nav'>
+                <div className="info-nav-topic">Architecture</div>
+                <div className="info-nav-topic">Component Graph</div>
+                <div className="info-nav-topic">Solutions</div>
+            </div>
             <div className='info-box'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque consequatur dignissimos nobis vitae odit facere pariatur quod, eum ex deserunt, rem, eaque est? Rerum magni, qui sit quas illo voluptatum?</p>
             </div>
