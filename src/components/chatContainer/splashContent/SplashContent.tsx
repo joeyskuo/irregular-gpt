b09eb8ff46d42bdb799894e814c9f5f7f0846ab6
Joey Kuo <joeyskuo@gmail.com>
2024-07-30 09:12:17 -0700

    splash-content--robot className for image

--- src/components/chatContainer/splashContent/SplashContent.tsx
+++ src/components/chatContainer/splashContent/SplashContent.tsx
@@ -5,7 +5,7 @@ const SplashContent = () => {
     return (
         <div className="splash-content">
             <div className="splash-content--prompt">How can I help you today?</div>
-            <img src="/assets/robot.png" />
+            <img className="splash-content--robot" src="/assets/robot.png" />
         </div>
     )
 }
