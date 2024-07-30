3a3c14f3200ef1524c3e2a17277a7159e5c0f892
Joey Kuo <joeyskuo@gmail.com>
2024-07-30 10:01:03 -0700

    info-box placeholder content

--- src/components/infoScreen/InfoScreen.tsx
+++ src/components/infoScreen/InfoScreen.tsx
@@ -3,8 +3,9 @@ import './InfoScreen.scss';
 const InfoScreen = () => {
     return (
         <div className="info-screen">
-            <div>Info</div>
-            <img className="info-button" src='/assets/info.svg' />
+            <div className='info-box'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque consequatur dignissimos nobis vitae odit facere pariatur quod, eum ex deserunt, rem, eaque est? Rerum magni, qui sit quas illo voluptatum?</p>
+            </div>
         </div>
     )
 }
