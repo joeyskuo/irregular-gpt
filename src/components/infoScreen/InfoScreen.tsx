ba1a2a0b5eeb28641c7607f8fc0e8875f96c8430
Joey Kuo <joeyskuo@gmail.com>
2024-07-30 13:50:39 -0700

    topic elements restructured

--- src/components/infoScreen/InfoScreen.tsx
+++ src/components/infoScreen/InfoScreen.tsx
@@ -4,9 +4,28 @@ const InfoScreen = () => {
     return (
         <div className="info-screen">
             <div className='info-nav'>
-                <div className="info-nav-topic">Architecture</div>
-                <div className="info-nav-topic">Component Graph</div>
-                <div className="info-nav-topic">Solutions</div>
+                <div className="info-nav-topic">
+                    <div className="info-nav-topic--title">Architecture</div>
+                    <div className="info-nav-item-container">
+                        <div className="info-nav-item">Overview</div>
+                    </div>
+                </div>
+                <div className="info-nav-topic">
+                    <div className="info-nav-topic--title">Frontend</div>
+                    <div className="info-nav-item-container">
+                        <div className="info-nav-item">Component Graph</div>
+                        <div className="info-nav-item">Server-Sent Events</div>
+                        <div className="info-nav-item">Repo: irregular-gpt</div>
+                    </div>
+                </div>
+                <div className="info-nav-topic">
+                    <div className="info-nav-topic--title">Backend</div>
+                    <div className="info-nav-item-container">
+                        <div className="info-nav-item">Inference</div>
+                        <div className="info-nav-item">Job Scheduling</div>
+                        <div className="info-nav-item">Repo: irregular-api</div>
+                    </div>
+                </div>
             </div>
             <div className='info-box'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque consequatur dignissimos nobis vitae odit facere pariatur quod, eum ex deserunt, rem, eaque est? Rerum magni, qui sit quas illo voluptatum?</p>
