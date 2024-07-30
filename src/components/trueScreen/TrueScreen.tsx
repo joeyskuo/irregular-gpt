d02c6addfb1da76e3fc1c647256d38ce8bf81f86
Joey Kuo <joeyskuo@gmail.com>
2024-07-29 23:17:06 -0700

    screenMode state onClick added

--- src/components/trueScreen/TrueScreen.tsx
+++ src/components/trueScreen/TrueScreen.tsx
@@ -6,11 +6,17 @@ import './TrueScreen.scss';
 const TrueScreen = () => {
 
     const [showInfo, setShowInfo] = useState<boolean>(false);
+    const [screenMode, setScreenMode] = useState<string>("default");
 
     return (
         <div className="true-screen">
             <SlimeScreen />
             {showInfo && <InfoScreen /> }
+            <div className="slime-button-container">
+                <img className="slime-button slime-button--mobile" src='/assets/mobile.svg' onClick={() => setScreenMode("mobile")}/>
+                <img className="slime-button slime-button--tablet" src='/assets/tablet.svg' onClick={() => setScreenMode("tablet")}/>
+                <img className="slime-button slime-button--desktop" src='/assets/desktop.svg' onClick={() => setScreenMode("desktop")}/>
+            </div>
             <img className="info-button" src='/assets/info.svg' onClick={() => setShowInfo(!showInfo)}/>
         </div>
     )
