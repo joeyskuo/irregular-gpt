7750227d87f1556d44fa8baf2146b31bd8152d1d
Joey Kuo <joeyskuo@gmail.com>
2024-07-29 20:31:15 -0700

    InfoScreen component created

---
+++ src/components/infoScreen/InfoScreen.tsx
@@ -0,0 +1,12 @@
+import './InfoScreen.scss';
+
+const InfoScreen = () => {
+    return (
+        <div className="info-screen">
+            <div>Info</div>
+            <img className="info-button" src='/assets/info.svg' />
+        </div>
+    )
+}
+
+export default InfoScreen;
