cb74af31db41e314d4ae9c9ecbe7fd44ac33fa6b
Joey Kuo <joeyskuo@gmail.com>
2024-07-29 22:46:50 -0700

    TrueScreen imported as root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SlimeScreen from './components/slimeScreen/SlimeScreen';
 import './_reset.css';
 import './index.css';
+import TrueScreen from './components/trueScreen/TrueScreen';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SlimeScreen/>
+    <TrueScreen />
   </React.StrictMode>,
 )
