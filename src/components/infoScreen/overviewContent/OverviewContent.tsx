e12fac78ede6ac704a776ee8600503a9968d5482
Joey Kuo <joeyskuo@gmail.com>
2024-07-31 13:06:35 -0700

    fix ul tag in p tag error

--- src/components/infoScreen/overviewContent/OverviewContent.tsx
+++ src/components/infoScreen/overviewContent/OverviewContent.tsx
@@ -6,8 +6,9 @@ const OverviewContent = () => {
         <div className="info-content--overview">
             <h3>Overview</h3>
             <img src="assets/overview.svg" />
-            <p>The client-side code is hosted as a static website on a S3 bucket while the backend is deployed on an EC2 instance. The instance has Nginx, Fastify, and Redis running.</p>
-            <p>The Fastify backend
+            <p>The client-side code is hosted as a static website on a S3 bucket while the backend is deployed on an EC2 instance. The EC2 instance has Nginx, Fastify, and Redis running.</p>
+            <div>
+                <p>The Fastify backend</p>
                 <ul>
                     <li>manages sessions between multiple concurrent users</li>
                     <li>configures the base settings for inference calls to the Claude API</li>
@@ -15,13 +16,14 @@ const OverviewContent = () => {
                     <li>runs scheduled jobs to clear sessions and cached conversations</li>
                     <li>makes inference calls to the Claude API and streams the response through server-sent events</li>
                 </ul>
-            </p>
-            <p>The React frontend
+            </div>
+            <div>
+                <p>The React frontend</p>
                 <ul>
                     <li>initializes a session with the backend</li>
                     <li>handles the rendering of incremental messages from server-sent events</li>
                 </ul>
-            </p>
+            </div>
 
         </div>
     )
